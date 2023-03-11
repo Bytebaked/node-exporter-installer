9c33cab734f35f0a23a41388502ffe7a70daef63
wynn <209275660@qq.com>
2023-03-11 12:08:11 +0000

    Hotfix

--- smart-exporter.sh
+++ smart-exporter.sh
@@ -36,7 +36,7 @@ systemctl start node_exporter.service
 curl -o /usr/local/bin/smartmon.sh https://raw.githubusercontent.com/Bytebaked/node-exporter-installer/main/assets/smartmon.sh
 chmod +x /usr/local/bin/smartmon.sh
 
-mkdir /var/lib/node_exporter/textfile_collector
+mkdir -p /var/lib/node_exporter/textfile_collector
 touch /var/lib/node_exporter/textfile_collector/smart_metrics.prom
 
 /usr/local/bin/smartmon.sh > /var/lib/node_exporter/textfile_collector/smart_metrics.prom
